79f7562d6fec4890837ce438c75bd3b9fb0bfd1b
lambda <shokhiabhiakalak@gmail.com>
2024-02-20 19:10:34 +0530

    echoserver done

--- project0/echoserver.py
+++ project0/echoserver.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python
 '''
-Simple echo server which handles 1 client. 
+A simple echo server
 '''
 import socket
-import sys
 
-# Create a tcp socket
-tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Fix error, address already in use
-tcpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind socket to an address
-host, port = sys.argv[1], int(sys.argv[2])
-address = (host, port)
-tcpSocket.bind(address)
-
-# Enable the server to accept connections
-tcpSocket.listen()
-
-print("Waiting for connection...", host, port)
-
-# Accept a connection
-(client, (clientIp, clientPort)) = tcpSocket.accept()
-
-print("Connected to client:", clientIp, "on port", clientPort)
-
-# Receive data from the socket
-data = "foo"
-while len(data):
-    data = client.recv(2048)
-    print(data.decode(), end='')
-
-print("Closing connection...")
-client.close()
-
-print("Shutting down the server...")
-tcpSocket.close()
-
-
-
+HOST = ''
+PORT = 1337
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpsocket:
+    tcpsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpsocket.bind((HOST, PORT))
+    tcpsocket.listen()
+    conn, addr = tcpsocket.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+           # print('Received data', len(data), 'bytes')
+           # print(type(data))
+            if not data: 
+                break
+            sent = conn.sendall(data)
